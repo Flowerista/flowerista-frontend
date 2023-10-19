2a63824515bf2c8b927de76652236b672b1052a4
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-10-19 17:11:50 +0300

    feat: add sections 'About Us' and 'Reviews'; connected fonts

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,6 @@
 import {FC} from 'react';
+import { AboutUs } from '../../components/AboutUs/AboutUs';
+import { Reviews } from '../../components/Reviews/Reviews';
 
 export interface IHomePage {
 }
@@ -6,7 +8,8 @@ export interface IHomePage {
 export const HomePage: FC<IHomePage> = () => {
 	return (
 		 <div>
-			 
+			 <AboutUs/>
+			 <Reviews/>
 		 </div>
 	);
 };
