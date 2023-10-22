ba512f19610d9f7999641fd008922eab83758ae7
Ihor <vitrikushihor0101@gmail.com>
2023-10-22 21:42:58 +0300

    add catalog in homePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,18 @@
 import {FC} from 'react';
-import { AboutUs } from '../../components/AboutUs/AboutUs';
-import { Reviews } from '../../components/Reviews/Reviews';
+import {AboutUs} from '../../components/AboutUs/AboutUs';
+import {Reviews} from '../../components/Reviews/Reviews';
 import {HomeSwiper} from './HomeSwiper';
-
+import {CatalogBox} from '../../components/CatalogBox';
+import styles from './styles.module.scss'
 
 export const HomePage: FC = () => {
 	return (
-     <main>
+     <main className={styles.home}>
        <HomeSwiper/>
+	     <div className={styles.home__catalog}>
+		     <CatalogBox text={"New arrivals"}/>
+		     <CatalogBox text={"Bouquet in a box"}/>
+	     </div>
 			 <AboutUs/>
 			 <Reviews/>
 		 </main>
