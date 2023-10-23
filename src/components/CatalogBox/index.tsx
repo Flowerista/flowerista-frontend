a9f64aeeaf039d1ed6402461300f7e9a4bf37aa1
Ihor <vitrikushihor0101@gmail.com>
2023-10-23 18:36:53 +0300

    added swiper images and search animation also catalog added links

--- src/components/CatalogBox/index.tsx
+++ src/components/CatalogBox/index.tsx
@@ -2,14 +2,16 @@ import {FC} from 'react';
 import styles from './styles.module.scss';
 import firstImg from '../../assets/image/catalog/first.png'
 import secondImg from '../../assets/image/catalog/second.png'
+import {NavLink} from 'react-router-dom';
+import {Route} from '../../data/routes';
 
 
 export const CatalogBox: FC<{text:string}> = ({text}) => {
 	return (
-		 <div className={styles.catalog}>
+		 <NavLink to={Route.Catalog} className={styles.catalog}>
 					<h2>{text}</h2>
 			 {text ==="New arrivals"? <img src={firstImg} alt="catalog img"/>:<img src={secondImg} alt="catalog img"/>}
-		 </div>
+		 </NavLink>
 	);
 };
 
--- src/components/ParametersMenu/Search/index.tsx
+++ src/components/ParametersMenu/Search/index.tsx
@@ -1,15 +1,22 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import styles from './styles.module.scss';
 import {BsSearch} from 'react-icons/bs';
 
-export interface ISearch {
-}
 
-export const Search: FC<ISearch> = () => {
+export const Search: FC = () => {
+	const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+	const toggleSearch = () => {
+		setIsExpanded(!isExpanded);
+	};
+
 	return (
 		 <form className={styles.search}>
-       <button><BsSearch style={{fontSize:"24px",cursor:"pointer"}}/></button>
-			 <input type="text" placeholder={"Flower"}/>
+			 <div className={`${styles.searchBox} ${isExpanded ? `${styles.expanded}` : ''}`}>
+				 <BsSearch  className={styles.searchIcon} onClick={toggleSearch} style={{fontSize:"24px",cursor:"pointer"}}/>
+				 <input className={styles.searchInput} type="text" placeholder={"Flower"}/>
+			 </div>
+
 		 </form>
 	);
 };
