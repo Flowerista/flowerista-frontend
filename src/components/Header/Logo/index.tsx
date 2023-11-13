45360a275d88d2fa58960be4f47af633ea74f0f6
Ihor <vitrikushihor0101@gmail.com>
2023-11-13 18:43:06 +0200

    changed logo website and changed links added new attribute target top

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -2,8 +2,8 @@ import {FC} from 'react';
 import styles from './styles.module.scss';
 import {Link} from 'react-router-dom';
 import {DataRoute} from '../../../data/routes';
-import headerLogo from '../../../assets/image/logo.svg'
-import footerLogo from '../../../assets/image/logo.png'
+import headerLogo from '../../../assets/image/logo/white_logo.png'
+import footerLogo from '../../../assets/image/logo/black_logo.png'
 
 export const Logo: FC<{type:string}> = ({type}) => {
 	return (
