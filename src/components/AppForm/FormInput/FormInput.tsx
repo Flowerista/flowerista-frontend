94cb8e6b200b40f77fa42ad0761ae8ef9500c569
Ihor <vitrikushihor0101@gmail.com>
2024-01-27 10:58:51 +0200

    fixed problem with writing in input

--- src/components/AppForm/FormInput/FormInput.tsx
+++ src/components/AppForm/FormInput/FormInput.tsx
@@ -2,34 +2,42 @@ import {FC, ReactNode, useState} from 'react';
 import styles from './styles.module.scss';
 
 interface InputProps {
-  label: string;
-  type: string;
-  defaultValue?: string;
-  placeholder: string;
-  register: any;
-  error?: string;
-  children?: ReactNode;
-  required?: boolean;
+	label: string;
+	type: string;
+	defaultValue?: string;
+	placeholder: string;
+	register: any;
+	error?: string;
+	children?: ReactNode;
+	required?: boolean;
 }
 
-const FormInput: FC<InputProps> = ({label, type, defaultValue, placeholder, register, error, children,required=true}) => {
-  const [value, setValue] = useState(defaultValue || '');
-  return (
-    <label className={styles.label}>
-        <input 
-          {...register}
-          className={`${styles.input} ${error ? styles.input_error : ''}`}
-          type={type}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          // placeholder={placeholder}
-          required={required}
-          {...register}
-        />
-        <span>{label}</span>
-        {children}
-    </label>
-  )
+const FormInput: FC<InputProps> = ({
+	                                   label,
+	                                   type,
+	                                   defaultValue,
+	                                   placeholder,
+	                                   register,
+	                                   error,
+	                                   children,
+	                                   required = true,
+                                   }) => {
+	const [value, setValue] = useState(defaultValue || '');
+	return (
+		 <label className={styles.label}>
+			 <input
+					{...register}
+					className={`${styles.input} ${error ? styles.input_error : ''}`}
+					type={type}
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					// placeholder={placeholder || ''}
+					required={required}
+			 />
+			 <span>{label}</span>
+			 {children}
+		 </label>
+	)
 }
 
 export default FormInput;
