207d740d5c88de4795fa1b8aee5acf649a97d07b
Ihor <vitrikushihor0101@gmail.com>
2024-02-20 16:07:53 +0200

    fixed links open

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -7,7 +7,7 @@ import footerLogo from '../../../assets/image/logo/black_logo.png'
 
 export const Logo: FC<{ type: string }> = ({type}) => {
 	return (
-		 <Link target={'_top'} to={DataRoute.Home} className={styles.logo}>
+		 <Link target="_top" to={DataRoute.Home} className={styles.logo}>
 			 {type === 'footer' ? <img src={footerLogo} alt="logo"/> : <img src={headerLogo} alt="logo"/>}
 		 </Link>
 	);
