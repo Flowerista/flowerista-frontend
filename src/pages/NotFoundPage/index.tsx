6e257c66916ee53493fb820d3e600aedf4af5271
VladislavKuzh <v.s.kuzheliev@gmail.com>
2024-03-28 17:49:06 +0200

    feat: develop mobile version 404 page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,28 +1,43 @@
 import {FC} from 'react';
 import styles from './styles.module.scss'
 import {Logo} from '../../components/Header/Logo';
-import {NavLink} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import {DataRoute} from '../../data/routes';
 import {useTranslation} from 'react-i18next';
+import { Button } from '../../components';
 
 export const NotFoundPage: FC = () => {
 	const {t} = useTranslation()
+	const navigate = useNavigate()
 	return (
-		 <main className={styles.notFound}>
-			 <div className={styles.notFound__container}>
-				 <div className={styles.notFound__header}>
-					 <Logo type={'header'}/>
-				 </div>
-				 <div className={styles.notFound__main}>
-					 <h1>{t('notFoundPage.title')}</h1>
-					 <span>{t('notFoundPage.sub_title')}</span>
-					 <p>{t('notFoundPage.description')}</p>
-					 <NavLink to={DataRoute.Catalog}>{t('notFoundPage.btn1')}</NavLink>
-					 <NavLink to={DataRoute.Home}>{t('notFoundPage.btn2')}</NavLink>
-				 </div>
-				 <div className={styles.notFound__footer}>{t('notFoundPage.rights')}</div>
-			 </div>
-		 </main>
+		<main className={styles.notFound}>
+			<div className={styles.notFound__container}>
+				<div className={styles.notFound__header}>
+					<div className={styles.logo_wrp}>
+						<Logo type='footer'/>
+					</div>
+				</div>
+				<div className={styles.notFound__main}>
+					<h2>{t('notFoundPage.title')}</h2>
+					<span>{t('notFoundPage.sub_title')}</span>
+					<p>{t('notFoundPage.description')}</p>
+					<div className={styles.btn_wrp}>
+						<Button 
+							text={t('notFoundPage.btn1')} 
+							onClick={() => navigate(DataRoute.Catalog)}
+							sizeMode='full'
+						/>
+            			<Button 
+							text={t('notFoundPage.btn2')} 
+							onClick={() => navigate(DataRoute.Home)} 
+							colorMode="white" 
+							className={styles.btn}
+							sizeMode='full'/>
+					</div>
+				</div>
+				<div className={styles.notFound__footer}>{t('notFoundPage.rights')}</div>
+			</div>
+		</main>
 	);
 };
 
