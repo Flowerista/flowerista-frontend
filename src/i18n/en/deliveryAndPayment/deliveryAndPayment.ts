f948fac07904397c78af1abccae3e61e2731b6e0
VladislavKuzh <v.s.kuzheliev@gmail.com>
2024-04-23 11:29:06 +0300

    fix: fix image and select for ProductPage

--- src/i18n/en/deliveryAndPayment/deliveryAndPayment.ts
+++ src/i18n/en/deliveryAndPayment/deliveryAndPayment.ts
@@ -4,7 +4,7 @@ export const deliveryAndPaymentEn = {
 	'text2': 'If you have any further questions or need additional information, you can reach out to our manager at +380 99 999 99 99.',
 	'delivery': {
 		'title': 'Delivery Terms:',
-		'text1': 'Free Delivery: Enjoy free delivery when your order totals 500 UAH or more',
+		'text1': 'Free Delivery: Enjoy free delivery when your order totals 500 USD or more',
 		'text2': 'City Courier Delivery: We offer citywide courier delivery to accommodate your needs at a convenient time and place',
 		'text3': 'Regional Courier Delivery: If your address is outside the city limits, we also provide delivery across the region to ensure the utmost convenience for your order.',
 		'text4': 'Self-departure from self-departure points: If you prefer to collect your order yourself, you can use the service of self-departure from the address 45 Peremohy St., Kyiv, Ukraine.',
