831c4cdeba3f2e4ff5a8991a859ef648febdbdea
VladislavKuzh <v.s.kuzheliev@gmail.com>
2024-05-02 22:50:30 +0300

    feat: add scroll in pagination

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,23 +1,23 @@
-import {FC, useEffect, useState} from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import {Pagination} from '../../components/Pagination/Pagination';
-import {Filters} from './Filters';
-import {useGetAllFlowersQuery} from '../../services/bouquete-api/bouquete-api-service';
-import {IFetchAllFlowers} from '../../interface/flower';
-import {Card} from '../../components/Card/Card';
-import {SkeletonCard} from '../../components/Skeletons/SkeletonCard/SkeletonCard';
-import {useAppSelector} from '../../store/store';
-import {useDebounce} from '../../hooks/useDebounce';
-import {Link} from 'react-router-dom';
-import {DataRoute} from '../../data/routes';
-import {useTranslation} from 'react-i18next';
+import { Pagination } from '../../components/Pagination/Pagination';
+import { Filters } from './Filters';
+import { useGetAllFlowersQuery } from '../../services/bouquete-api/bouquete-api-service';
+import { IFetchAllFlowers } from '../../interface/flower';
+import { Card } from '../../components/Card/Card';
+import { SkeletonCard } from '../../components/Skeletons/SkeletonCard/SkeletonCard';
+import { useAppSelector } from '../../store/store';
+import { useDebounce } from '../../hooks/useDebounce';
+import { Link } from 'react-router-dom';
+import { DataRoute } from '../../data/routes';
+import { useTranslation } from 'react-i18next';
 
 export interface ICatalogPage {
 }
 
 const CatalogPage: FC<ICatalogPage> = () => {
-	const {t} = useTranslation()
-	const {filters} = useAppSelector(state => state.filtration)
+	const { t } = useTranslation()
+	const { filters } = useAppSelector(state => state.filtration)
 	const debouncedMinPrice = useDebounce<number>(filters.minPrice, 500)
 	const debouncedMaxPrice = useDebounce<number>(filters.maxPrice, 500)
 
@@ -41,11 +41,12 @@ const CatalogPage: FC<ICatalogPage> = () => {
 
 	const [dataState, setDataState] = useState(dataFetch)
 
-	const {data, error} = useGetAllFlowersQuery(dataState)
+	const { data, error } = useGetAllFlowersQuery(dataState)
 
 	const handlePageClick = (event: any) => {
 		const newPage = event.selected + 1
-		setDataState((state) => ({...state, page: newPage}))
+		setDataState((state) => ({ ...state, page: newPage }))
+		window.scrollTo({ top: 0, behavior: 'smooth' });
 	}
 
 	if (error) {
@@ -53,41 +54,41 @@ const CatalogPage: FC<ICatalogPage> = () => {
 	}
 
 	return (
-		 <div className={styles.catalog}>
-			 <div className={styles.catalog__name}><Link to={DataRoute.Home}>{t('catalog.link')}</Link>{t('catalog.link2')}
-			 </div>
-			 <Filters/>
-			 {data ?
-					<div className={styles.flower__wrapper}>
-						{data && data.content.map(item => (
-							 <Card
-									id={item.id}
-									name={item.name}
-									discount={item.discount}
-									defaultPrice={item.defaultPrice}
-									discountPrice={item.discountPrice}
-									imageUrls={item.imageUrls}
-									key={item.id}
-									sizes={item.sizes}
-							 />
-						))}
-					</div> :
-					<div
-						 style={{display: 'flex', marginTop: '50px', flexWrap: 'wrap', rowGap: '100px', marginBottom: '120px'}}>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-						<SkeletonCard/>
-					</div>
-			 }
-			 {data && <Pagination data={data} handlePageClick={handlePageClick}/>}
-		 </div>
+		<div className={styles.catalog}>
+			<div className={styles.catalog__name}><Link to={DataRoute.Home}>{t('catalog.link')}</Link>{t('catalog.link2')}
+			</div>
+			<Filters />
+			{data ?
+				<div className={styles.flower__wrapper}>
+					{data && data.content.map(item => (
+						<Card
+							id={item.id}
+							name={item.name}
+							discount={item.discount}
+							defaultPrice={item.defaultPrice}
+							discountPrice={item.discountPrice}
+							imageUrls={item.imageUrls}
+							key={item.id}
+							sizes={item.sizes}
+						/>
+					))}
+				</div> :
+				<div
+					style={{ display: 'flex', marginTop: '50px', flexWrap: 'wrap', rowGap: '100px', marginBottom: '120px' }}>
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+					<SkeletonCard />
+				</div>
+			}
+			{data && <Pagination data={data} handlePageClick={handlePageClick} />}
+		</div>
 	);
 };
 
