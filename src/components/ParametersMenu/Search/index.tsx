cbfbd18030abf7cbbe7d930941a880316e15ee40
Ihor <vitrikushihor0101@gmail.com>
2024-05-02 18:18:23 +0300

    fixed search and trying fix price

--- src/components/ParametersMenu/Search/index.tsx
+++ src/components/ParametersMenu/Search/index.tsx
@@ -1,24 +1,25 @@
-import { FC, FormHTMLAttributes, SetStateAction, useEffect, useState } from 'react';
+import {FC, FormHTMLAttributes, SetStateAction, useEffect, useRef, useState} from 'react';
 import styles from './styles.module.scss';
-import { BsSearch } from 'react-icons/bs';
-import { useSearchBouquetesQuery } from '../../../services/bouquete-api/bouquete-api-service';
-import { useDebounce } from '../../../hooks/useDebounce';
-import { Link } from 'react-router-dom';
-import { DataRoute } from '../../../data/routes';
+import {BsSearch} from 'react-icons/bs';
+import {useSearchBouquetesQuery} from '../../../services/bouquete-api/bouquete-api-service';
+import {useDebounce} from '../../../hooks/useDebounce';
+import {Link} from 'react-router-dom';
+import {DataRoute} from '../../../data/routes';
 import classNames from 'classnames';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 interface SearchProps extends FormHTMLAttributes<HTMLFormElement> {
 	type: 'header' | 'menu'
 }
 
 
-export const Search: FC<SearchProps> = ({ className, type }) => {
-	const { t } = useTranslation()
+export const Search: FC<SearchProps> = ({className, type}) => {
+	const {t} = useTranslation()
 	const [isExpanded, setIsExpanded] = useState<boolean>(false);
 	const [searchTerm, setSearchTerm] = useState<string>('');
 	const debouncedSearch = useDebounce(searchTerm, 500)
-	const { data } = useSearchBouquetesQuery(debouncedSearch)
+	const ref = useRef<HTMLDivElement | null>(null)
+	const {data} = useSearchBouquetesQuery(debouncedSearch)
 
 	useEffect(() => {
 		if (type === 'menu') {
@@ -26,9 +27,23 @@ export const Search: FC<SearchProps> = ({ className, type }) => {
 		}
 	}, [])
 
+	useEffect(() => {
+		if (type === 'header') {
+			document.addEventListener('click', handleClickOutside, true)
+			return () => {
+				document.removeEventListener('click', handleClickOutside, true)
+			}
+		}
+	})
 
 	const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
 		setSearchTerm(e.target.value);
+	}
+
+	const handleClickOutside = (event: any) => {
+		if (ref.current && !ref.current.contains(event.target)) {
+			setIsExpanded(false)
+		}
 	}
 
 	const toggleSearch = () => {
@@ -38,40 +53,40 @@ export const Search: FC<SearchProps> = ({ className, type }) => {
 	};
 
 	return (
-		<form className={styles.search}>
-			<div className={classNames(styles.searchBox, { [styles.expanded]: isExpanded }, styles[type], className)}>
-				<BsSearch className={styles.searchIcon} onClick={toggleSearch} style={{ fontSize: '24px', cursor: 'pointer' }} />
-				<input
-					className={styles.searchInput}
-					type="text"
-					placeholder={`${t('header.search')}`}
-					value={searchTerm}
-					onChange={handleInputChange}
-				/>
-			</div>
-			{data && data.length > 0 ? (
-				<ul className={classNames(styles.searchResult, styles[type])} style={{ display: !isExpanded ? 'none' : '' }}>
-					{data.map((result) => (
-						<>
-							<li key={result.id}>
+		 <form className={styles.search}>
+			 <div ref={ref}
+			      className={classNames(styles.searchBox, {[styles.expanded]: isExpanded}, styles[type], className)}>
+				 <BsSearch className={styles.searchIcon} onClick={toggleSearch} style={{fontSize: '24px', cursor: 'pointer'}}/>
+				 <input
+						className={styles.searchInput}
+						type="text"
+						placeholder={`${t('header.search')}`}
+						value={searchTerm}
+						onChange={handleInputChange}
+				 />
+			 </div>
+			 {data && data.length > 0 ? (
+					<ul className={classNames(styles.searchResult, styles[type])} style={{display: !isExpanded ? 'none' : ''}}>
+						{data.map((result) => (
+							 <>
+								 <li key={result.id}>
 
-								<Link to={`${DataRoute.Product}/${result.id}`}>
-									<img src={result.imageUrls?.['1']} alt="" />
-									<div className={styles.searchResult__content}>
-										<span>{result.name}</span>
-										<div>
-											{result.discountPrice ? <p className={styles.priceDiscount}>{result.defaultPrice} USD</p> : ''}
-											<p className={styles.defaultPrice}>{result.discountPrice ? result.discountPrice : result.defaultPrice} USD</p>
-										</div>
-									</div>
-								</Link>
-							</li>
-							<div className={styles.full}></div>
-						</>
-					))}
-				</ul>
-			) : null}
-		</form>
+									 <Link to={`${DataRoute.Product}/${result.id}`}>
+										 <img src={result.imageUrls?.['1']} alt=""/>
+										 <div className={styles.searchResult__content}>
+											 <span>{result.name}</span>
+											 <div>
+												 {result.discountPrice ? <p className={styles.priceDiscount}>{result.defaultPrice} USD</p> : ''}
+												 <p className={styles.defaultPrice}>{result.discountPrice ? result.discountPrice : result.defaultPrice} USD</p>
+											 </div>
+										 </div>
+									 </Link>
+								 </li>
+								 <div className={styles.full}></div>
+							 </>
+						))}
+					</ul>
+			 ) : null}
+		 </form>
 	);
 };
-
