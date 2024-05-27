3c671ef60a835ac69bce4e092c6f9de0bb2d0ec4
Ihor <vitrikushihor0101@gmail.com>
2024-05-27 20:03:13 +0300

    try fix deploy

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   e2e: {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    setupNodeEvents() {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000/'
